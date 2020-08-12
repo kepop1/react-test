6dfcf039055a2b3b61f7a932a314fa9a6fd20b4b
kepop1 <kepop123@gmail.com>
2020-08-12 21:42:05 +0100

    Updates

---
+++ precache-manifest.4d725d3e1d80c430669ec7e07a1ed207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e36ca5d747f6f5f91c9baf0368934ce",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "440192c8a1a60c9f4cc8",
+    "url": "/react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7abe38b47505c42d57d2",
+    "url": "/react-test/static/js/2.c54a9235.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.c54a9235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "440192c8a1a60c9f4cc8",
+    "url": "/react-test/static/js/main.f85e4112.chunk.js"
+  },
+  {
+    "revision": "a73b77d85a0436678cfa",
+    "url": "/react-test/static/js/runtime-main.a3de2790.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
